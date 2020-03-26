7158740e4fd5805871825af43390424d2f2075c6
IlliaPelypenko <35105041+illiapelypenko@users.noreply.github.com>
2020-03-26 04:10:13 +0200

    change port

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  port: 5000,
-  mongodb: "mongodb+srv://Atro:core4690@florist-c6cy6.mongodb.net/florist"
+	mongodb: 'mongodb+srv://Atro:core4690@florist-c6cy6.mongodb.net/florist'
 };
