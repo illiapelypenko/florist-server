6606f53183a7b8b10574667a224042935ce01b69
illia_pelypenko <pelypenkoillya@gmail.com>
2020-07-05 04:10:49 +0300

    setting up aws s3

--- aws-test.js
+++ aws-test.js
@@ -1,25 +1,29 @@
 const fs = require('fs');
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-const uploadFile = fileName => {
-	// Read content from the file
-	const fileContent = fs.readFileSync(__dirname + '\\' + fileName);
+const accessKeyId = 'AKIAIGFNDLM2DI4KEZBQ';
+const secretAccessKey = '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';
 
-	// Setting up S3 upload parameters
-	const params = {
-		Bucket: 'florist-images',
-		Key: fileName, // File name you want to save as in S3
-		Body: fileContent
-	};
+const s3 = new AWS.S3({ accessKeyId, secretAccessKey });
 
-	// Uploading files to the bucket
-	s3.upload(params, function(err, data) {
-		if (err) {
-			throw err;
-		}
-		console.log(data.Location);
-	});
+const uploadFile = (fileName) => {
+  // Read content from the file
+  const fileContent = fs.readFileSync(__dirname + '/' + fileName);
+
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: 'florist-images',
+    Key: fileName, // File name you want to save as in S3
+    Body: fileContent,
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function (err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(data.Location);
+  });
 };
 
-uploadFile('img0.jpeg');
+// uploadFile('flower2.jpeg');
--- routes/items.js
+++ routes/items.js
@@ -4,91 +4,118 @@ const fs = require('fs');
 const router = express.Router();
 const absolutePath = require('path');
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+const accessKeyId = 'AKIAIGFNDLM2DI4KEZBQ';
+const secretAccessKey = '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';
+const s3 = new AWS.S3({ accessKeyId, secretAccessKey });
 // const imagesStore = `${__dirname}/../items-images`;
 
 router.get('/all', async (req, res) => {
-	try {
-		let items = await Item.find();
-		items = items
-			.map(item => {
-				return ({ _id, name, price, data, fileName, type, birthtimeMs } = item);
-			})
-			.reverse();
-		res.json(items);
-	} catch (e) {
-		res.status(400).send('server error');
-		console.log(e);
-	}
-});
-
-router.get('/picture/:id', async (req, res) => {
-	try {
-		const file = await Item.findOne({ _id: req.params.id });
-		res.status(200).end(file.data, 'binary');
-	} catch (e) {
-		res.status(400).send('server error');
-		console.log(e);
-	}
-});
-
-router.delete('/delete/:id', async (req, res) => {
-	try {
-		Item.deleteOne({ _id: req.params.id }, err => console.log);
-		res.send();
-	} catch (e) {
-		res.status(400).send('server error');
-		console.log(e);
-	}
-});
-
-router.post('/upload', async (req, res) => {
-	try {
-		const file = req.files.file;
-		const name = req.body.name;
-		const price = req.body.price;
-		const fileName = file.name;
-		const type = req.body.type;
-		const birthtimeMs = new Date().getTime();
-		const data = file.data;
-		let newItem = new Item({
-			name,
-			price,
-			data,
-			fileName,
-			type,
-			birthtimeMs,
-		});
-
-		await newItem.save();
-
-		res.status(200).send();
-	} catch (e) {
-		res.status(400).send('server error');
-		console.log(e);
-	}
-});
-
-router.post('/test', async (req, res) => {
   try {
-		const file = req.files.file;
-		const data = file.data;
-		const params = {
-      Bucket: 'florist-images',
-      Key: 'test-img',
-      Body: data,
-		};
-		s3.upload(params, async function (err, data) {
-      if (err) {
-        throw err;
-      }
-      res.status(200).send(data);
-    });
-		res.status(200).end(data, 'binary');
+    let items = await Item.find();
+    items = items
+      .map((item) => {
+        return ({ _id, name, price, data, fileName, type, birthtimeMs } = item);
+      })
+      .reverse();
+    res.json(items);
   } catch (e) {
     res.status(400).send('server error');
     console.log(e);
   }
 });
+
+router.get('/picture/:id', async (req, res) => {
+  try {
+    const file = await Item.findOne({ _id: req.params.id });
+    res.status(200).end(file.data, 'binary');
+  } catch (e) {
+    res.status(400).send('server error');
+    console.log(e);
+  }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+  try {
+    Item.deleteOne({ _id: req.params.id }, (err) => console.log);
+    res.send();
+  } catch (e) {
+    res.status(400).send('server error');
+    console.log(e);
+  }
+});
+
+router.post('/upload', async (req, res) => {
+  try {
+    const file = req.files.file;
+    const name = req.body.name;
+    const price = req.body.price;
+    const fileName = file.name;
+    const type = req.body.type;
+    const birthtimeMs = new Date().getTime();
+    const data = file.data;
+    let newItem = new Item({
+      name,
+      price,
+      data,
+      fileName,
+      type,
+      birthtimeMs,
+    });
+
+    await newItem.save();
+
+    res.status(200).send();
+  } catch (e) {
+    res.status(400).send('server error');
+    console.log(e);
+  }
+});
+
+router.post('/upload-test', async (req, res) => {
+  try {
+    const file = req.files.file;
+    const fileName = file.name;
+    const data = file.data;
+
+    const params = {
+      Bucket: 'florist-images',
+      Key: fileName, // File name you want to save as in S3
+      Body: data, // Buffer
+    };
+
+    s3.upload(params, function (err, data) {
+      if (err) {
+        throw err;
+      }
+
+      res.status(200).send(data.Location);
+    });
+  } catch (e) {
+    res.status(400).send('server error');
+    console.log(e);
+  }
+});
+
+// router.post('/test', async (req, res) => {
+//   try {
+// 		const file = req.files.file;
+// 		const data = file.data;
+// 		const params = {
+//       Bucket: 'florist-images',
+//       Key: 'test-img',
+//       Body: data,
+// 		};
+// 		s3.upload(params, async function (err, data) {
+//       if (err) {
+//         throw err;
+//       }
+//       res.status(200).send(data);
+//     });
+// 		res.status(200).end(data, 'binary');
+//   } catch (e) {
+//     res.status(400).send('server error');
+//     console.log(e);
+//   }
+// });
 
 module.exports = router;
